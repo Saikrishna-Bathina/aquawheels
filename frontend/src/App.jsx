bfb319ddd080d7ad84df030f23ea0cfa9980a0d7
Sai Krishna Bathina <saikrishna.bathina999@gmail.com>
2025-07-17 16:54:02 +0530

    intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+const App = () => {
+  return (
+    <div className="h-screen bg-gray-800 flex items-center justify-center">
+      <h1 className="text-4xl text-green-400 font-bold">Tailwind is working!</h1>
+    </div>
+  );
+};
+
+export default App;
